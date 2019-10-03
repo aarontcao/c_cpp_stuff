ea9c3022915e38a79639d495b98f9aa585197823
aarontcao <aarontcao@gmail.com>
2019-10-03 00:49:37 -0400

    did some stuff idk man

---
+++ QuantNetCpp/Level 5/Section 3.4/3.4.2/Shape.cpp
@@ -0,0 +1,102 @@
+/*
+	C++ code for HW5, Section 3.4, Exercise 2
+*/
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include "Circle.hpp"
+#include "Point.hpp"
+#include <sstream>
+#include <iostream>
+
+// Default constructor
+Circle::Circle(): center(0, 0), radius(1)
+{
+	cout << "Default Constructor" << endl;
+}
+
+// Constructor with center, radius
+Circle::Circle(const Point& c, double r): center(c), radius(r)
+{
+	cout << "Constructor" << endl;
+}
+
+// Copy constructor
+Circle::Circle(const Circle& c): center(c.center), radius(c.radius)
+{
+	cout << "Copy Constructor" << endl;
+}
+
+// Destructor
+Circle::~Circle()
+{
+	cout << "Destructor" << endl;
+}
+
+// Modifier function for center
+void Circle::CentrePoint(const Point& c)
+{
+	center = c;
+}
+
+// Modifier function for radius
+void Circle::Radius(double r)
+{
+	radius = r;
+}
+
+// Selector functions for center, radius
+Point Circle::CentrePoint() const
+{
+	return center;
+}
+
+double Circle::Radius() const
+{
+	return radius;
+}
+
+// Diameter of circle
+double Circle::Diameter() const 
+{
+	return 2.0 * radius;
+}
+
+// Area of circle
+double Circle::Area() const
+{
+	return M_PI * pow(radius, 2);
+}
+
+// Circumference of circle
+double Circle::Circumference() const 
+{
+	return 2.0 * M_PI * radius;
+}
+
+// Convert circle to string
+string Circle::toString() const 
+{
+	stringstream stream;
+
+	stream << "Center: " << center.toString() << ", " << "Radius: " << radius;
+
+	return stream.str();
+}
+
+// Assignment operator
+Circle& Circle::operator = (const Circle& source)
+{
+	center = source.center;
+	radius = source.radius;
+
+	return *this;
+}
+
+// Send to ostream
+ostream& operator << (ostream& os, const Circle& c)
+{
+	os << c.toString() << endl;
+
+	return os;
+}
---
+++ QuantNetCpp/Level 5/Section 3.4/3.4.2/Shape.hpp
@@ -0,0 +1,55 @@
+/*
+	Header file for HW5, Section 3.4, Exercise 2
+*/
+
+#ifndef Circle_HPP
+#define Circle_HPP
+
+#include "Point.hpp"
+
+class Circle {
+private:
+	Point center;
+	double radius;
+
+public:
+	// Default constructor
+	Circle();
+
+	// Constructor with center, radius
+	Circle(const Point& c, double r);
+
+	// Copy constructor
+	Circle(const Circle& c);
+
+	// Destructor
+	~Circle();
+
+	// Selector functions for center, radius
+	Point CentrePoint() const;
+	double Radius() const;
+
+	// Modifier functions for center, radius
+	void CentrePoint(const Point& c);
+	void Radius(double r);
+
+	// Diameter of circle
+	double Diameter() const;
+
+	// Area of circle
+	double Area() const;
+
+	// Circumference of circle
+	double Circumference() const;
+
+	// Convert circle to string
+	string toString() const;
+
+	// Assignment operator
+	Circle& operator = (const Circle& source);
+};
+
+// Send to ostream
+ostream& operator << (ostream& os, const Circle& c);
+
+#endif
